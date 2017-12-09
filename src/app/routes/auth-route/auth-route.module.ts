20d553485d3273c5c03ca9a66c5dbda09ee8201c
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-09 21:00:46 +0100

    feat(auth-route): add core module as import

--- src/app/routes/auth-route/auth-route.module.ts
+++ src/app/routes/auth-route/auth-route.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ROUTES } from './auth-route.routes';
 import { RouterModule } from '@angular/router';
 import { AuthRouteComponent } from './auth-route.component';
+import { BolCoreModule } from '../../modules/bol-core/bol-core.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    BolCoreModule,
     RouterModule.forChild(ROUTES)
   ],
   declarations: [
