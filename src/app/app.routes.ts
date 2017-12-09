a57327d4c82955a1c6e17093d9539bdac65d85cd
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-09 21:01:58 +0100

    feat(app-routes): add games and game routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,11 @@
 export const ROUTES = [
   {
     path: 'auth', loadChildren: './routes/auth-route/auth-route.module#AuthRouteModule'
+  },
+  {
+    path: 'games', loadChildren: './routes/games-route/games-route.module#GamesRouteModule'
+  },
+  {
+    path: 'game', loadChildren: './routes/game-route/game-route.module#GameRouteModule'
   }
 ];
