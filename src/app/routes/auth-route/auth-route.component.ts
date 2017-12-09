75d3389f0bbaadde7dad03270b7e9f160a69250d
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-09 22:49:48 +0100

    feat(auth-route): full styled blueprint

--- src/app/routes/auth-route/auth-route.component.ts
+++ src/app/routes/auth-route/auth-route.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bol-auth-route',
-  templateUrl: './auth-route.component.html',
-  styleUrls: ['./auth-route.component.scss']
+    selector: 'bol-auth-route',
+    templateUrl: './auth-route.component.html',
+    styleUrls: ['./auth-route.component.scss']
 })
 export class AuthRouteComponent implements OnInit {
 
-  constructor() { }
+    userName: string;
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    loginUser() {
+
+    }
 
 }
