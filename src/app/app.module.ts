ac46e58e8223265adeef8358eed8089d858b94d4
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-10 23:19:59 +0100

    feat(app-routes): add auth guard to game & games routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppComponent } from './app.component';
 import { AuthRouteComponent } from './routes/auth-route/auth-route.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './app.routes';
+import { BolCoreModule } from './modules/bol-core/bol-core.module';
+import { AuthGuard } from './guards/auth.guard';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(ROUTES)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BolCoreModule,
+        RouterModule.forRoot(ROUTES)
+    ],
+    providers: [
+        AuthGuard
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,19 @@
+import { AuthGuard } from './guards/auth.guard';
+
 export const ROUTES = [
-  {
-    path: 'auth', loadChildren: './routes/auth-route/auth-route.module#AuthRouteModule'
-  },
-  {
-    path: 'games', loadChildren: './routes/games-route/games-route.module#GamesRouteModule'
-  },
-  {
-    path: 'game', loadChildren: './routes/game-route/game-route.module#GameRouteModule'
-  }
+    {
+        path: 'auth', loadChildren: './routes/auth-route/auth-route.module#AuthRouteModule'
+    },
+    {
+        path: 'games', loadChildren: './routes/games-route/games-route.module#GamesRouteModule',
+        canActivate: [
+            AuthGuard
+        ]
+    },
+    {
+        path: 'game', loadChildren: './routes/game-route/game-route.module#GameRouteModule',
+        canActivate: [
+            AuthGuard
+        ]
+    }
 ];
