cced65fbb963432251e2251f54bb0261cc8db555
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-10 19:15:19 +0100

    feat(auth-route-component): add logInUser

--- src/app/routes/auth-route/auth-route.component.ts
+++ src/app/routes/auth-route/auth-route.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../modules/bol-core/services/auth.service';
+import { LoggerFactoryService, Logolous } from '../../modules/bol-core/services/logger-factory.service';
 
 @Component({
     selector: 'bol-auth-route',
@@ -8,15 +10,25 @@ import { Component, OnInit } from '@angular/core';
 export class AuthRouteComponent implements OnInit {
 
     userName: string;
+    logInInProgress: boolean;
+    logger: Logolous;
 
-    constructor() {
+    constructor(private loggerFactoryService: LoggerFactoryService, private authService: AuthService) {
+        this.logger = loggerFactoryService.make('AuthRouteComponent');
     }
 
     ngOnInit() {
     }
 
-    loginUser() {
-
+    logInUser() {
+        if (this.logInInProgress) return;
+        this.logInInProgress = true;
+        this.authService.logIn(this.userName).then(() => {
+            this.logger.info(`${this.userName} was logged in!`);
+        }).catch((err) => {
+            this.logger.error(`Could not log in "${this.userName}" with error:`, err);
+        }).finally(() => {
+            this.logInInProgress = false;
+        });
     }
-
 }
