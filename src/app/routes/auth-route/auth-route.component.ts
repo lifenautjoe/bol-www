4ef5e57f27b31c2a233b0bf1db7cc26c4da4467b
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-10 23:18:14 +0100

    feat(auth-route-component): finish auth flow

--- src/app/routes/auth-route/auth-route.component.ts
+++ src/app/routes/auth-route/auth-route.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../modules/bol-core/services/auth.service';
 import { LoggerFactoryService, Logolous } from '../../modules/bol-core/services/logger-factory.service';
+import { RouterHelperService } from '../../modules/bol-core/services/router-helper.service';
 
 @Component({
     selector: 'bol-auth-route',
@@ -12,21 +13,34 @@ export class AuthRouteComponent implements OnInit {
     userName: string;
     logInInProgress: boolean;
     logger: Logolous;
+    errorMessage: string;
 
-    constructor(private loggerFactoryService: LoggerFactoryService, private authService: AuthService) {
+    constructor(private loggerFactoryService: LoggerFactoryService,
+                private authService: AuthService,
+                private routerHelperService: RouterHelperService) {
         this.logger = loggerFactoryService.make('AuthRouteComponent');
     }
 
     ngOnInit() {
+
     }
 
     logInUser() {
+        debugger;
         if (this.logInInProgress) return;
         this.logInInProgress = true;
         this.authService.logIn(this.userName).then(() => {
-            this.logger.info(`${this.userName} was logged in!`);
+            this.logger.info(`${this.userName} was logged in! Sending to games`);
+            debugger;
+            return this.routerHelperService.goToGames().then(() => {
+                this.logger.info('sent!');
+            }).catch((err) => {
+                this.logger.info(err);
+            })
         }).catch((err) => {
+            debugger;
             this.logger.error(`Could not log in "${this.userName}" with error:`, err);
+            this.errorMessage = err.error.message;
         }).finally(() => {
             this.logInInProgress = false;
         });
