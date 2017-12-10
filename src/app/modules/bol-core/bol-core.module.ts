929de1b6190ccf159a6a9d17b79a0541f3df1a05
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-10 19:16:02 +0100

    feat(bol-core-module): add created services

--- src/app/modules/bol-core/bol-core.module.ts
+++ src/app/modules/bol-core/bol-core.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './services/auth.service';
+import { AuthApiService } from './services/auth-api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { PromisedHttpService } from './services/promised-http.service';
+import { LoggerFactoryService } from './services/logger-factory.service';
 
 @NgModule({
-  providers: [
-    AuthService
-  ],
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    providers: [
+        AuthService,
+        PromisedHttpService,
+        AuthApiService,
+        AuthService,
+        LoggerFactoryService
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule
+    ],
+    declarations: []
 })
 export class BolCoreModule {
 }
--- src/app/routes/game-route/game-route.module.ts
+++ src/app/routes/game-route/game-route.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { ROUTES } from './game-route.routes';
 import { RouterModule } from '@angular/router';
 import { GameRouteComponent } from './game-route.component';
+import { BolCoreModule } from '../../modules/bol-core/bol-core.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(ROUTES)
-  ],
-  declarations: [
-    GameRouteComponent
-  ]
+    imports: [
+        CommonModule,
+        BolCoreModule,
+        RouterModule.forChild(ROUTES)
+    ],
+    declarations: [
+        GameRouteComponent
+    ]
 })
 export class GameRouteModule {
 }
