5efbb45fad958cb8fa22ceacdd09645d4e7cf88b
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-11 23:46:56 +0100

    feat(games-route-module): add new components and pipe

--- src/app/routes/games-route/games-route.module.ts
+++ src/app/routes/games-route/games-route.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ROUTES } from './games-route.routes';
 import { RouterModule } from '@angular/router';
 import { GamesRouteComponent } from './games-route.component';
+import { SearchResultGameComponent } from './components/search-result-game/search-result-game.component';
+import { SearchGamePipe } from './pipes/search-game.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(ROUTES)
-  ],
-  declarations: [
-    GamesRouteComponent
-  ]
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(ROUTES)
+    ],
+    declarations: [
+        GamesRouteComponent,
+        SearchResultGameComponent,
+        SearchGamePipe
+    ]
 })
 export class GamesRouteModule {
+
 }
