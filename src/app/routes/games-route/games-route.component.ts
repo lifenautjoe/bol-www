d3bc995f2d460d65abd8a7c228db53c3b6cdc429
Joel Hernandez <lifenautjoe@gmail.com>
2017-12-11 23:44:25 +0100

    feat(games-route-component): finish all programmatic behaviour

--- src/app/routes/games-route/games-route.component.ts
+++ src/app/routes/games-route/games-route.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Game } from '../../modules/bol-core/models/game';
+import * as Bluebird from 'bluebird';
+import { GamesService } from '../../modules/bol-core/services/games.service';
 
 @Component({
-  selector: 'bol-games-route',
-  templateUrl: './games-route.component.html',
-  styleUrls: ['./games-route.component.scss']
+    selector: 'bol-games-route',
+    templateUrl: './games-route.component.html',
+    styleUrls: ['./games-route.component.scss']
 })
-export class GamesRouteComponent implements OnInit {
+export class GamesRouteComponent implements OnInit, OnDestroy {
+    private static readonly REFRESH_GAMES_INTERVAL = 5000;
+    // We filter the games list by this string
+    gamesFilter: string;
+    games: Array<Game>;
+    gamesRefreshInProgress: boolean;
 
-  constructor() { }
+    private intervalHandle;
 
-  ngOnInit() {
-  }
+    constructor(private gamesService: GamesService) {
+
+    }
+
+    ngOnInit() {
+        this.refreshGames().then(() => {
+            this.intervalHandle = setInterval(() => {
+                this.refreshGames();
+            }, GamesRouteComponent.REFRESH_GAMES_INTERVAL);
+        })
+    }
+
+    ngOnDestroy() {
+        if (this.intervalHandle) clearInterval(this.intervalHandle);
+    }
+
+    refreshGames(): Bluebird<void> {
+        this.gamesRefreshInProgress = true;
+        return this.getGames().then((games) => {
+            this.games = games;
+        }).finally(() => {
+            this.gamesRefreshInProgress = false;
+        });
+    }
+
+    getGames(): Bluebird<Array<Game>> {
+        return this.gamesService.getGames();
+    }
 
 }
